14278b76bcef44e4b47a34b7e4a68f2a6fe03547
Amir <amir.nagri+git@gmail.com>
2015-08-30 03:28:29 +0530

    ui

---
+++ app/src/main/java/co/creativev/smartcar/Command.java
@@ -0,0 +1,7 @@
+package co.creativev.smartcar;
+
+public class Command {
+    public static String run(String input) {
+
+    }
+}
--- app/src/main/java/co/creativev/smartcar/MainActivity.java
+++ app/src/main/java/co/creativev/smartcar/MainActivity.java
@@ -1,24 +1,13 @@
 package co.creativev.smartcar;
 
-import android.app.AlertDialog;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
 
-import java.io.IOException;
-import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Set;
-import java.util.UUID;
 
-public class MainActivity extends AppCompatActivity implements DialogInterface.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     private ArrayList devices;
 
@@ -26,25 +15,6 @@ public class MainActivity extends AppCompatActivity implements DialogInterface.O
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ArrayList deviceStrs = new ArrayList();
-        devices = new ArrayList();
-
-        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
-        Set pairedDevices = btAdapter.getBondedDevices();
-        if (pairedDevices.size() > 0) {
-            for (Object device : pairedDevices) {
-                BluetoothDevice bdevice = (BluetoothDevice) device;
-                deviceStrs.add(bdevice.getName() + "\n" + bdevice.getAddress());
-                devices.add(bdevice.getAddress());
-            }
-        }
-
-        final AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.select_dialog_singlechoice,
-                deviceStrs.toArray(new String[deviceStrs.size()]));
-        alertDialog.setSingleChoiceItems(adapter, -1, this);
 
     }
 
@@ -70,21 +40,4 @@ public class MainActivity extends AppCompatActivity implements DialogInterface.O
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onClick(DialogInterface dialog, int which) {
-        dialog.dismiss();
-        int position = ((AlertDialog) dialog).getListView().getCheckedItemPosition();
-        String deviceAddress = devices.get(position).toString();
-        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
-        BluetoothDevice device = btAdapter.getRemoteDevice(deviceAddress);
-        UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");//AA:BB:CC:11:22:33");
-        try {
-            BluetoothSocket socket = device.createInsecureRfcommSocketToServiceRecord(uuid);
-            socket.connect();
-
-        } catch (IOException e) {
-            Log.e("ERROR", "Error", e);
-        }
-
-    }
 }
